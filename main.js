6ed54ebc6331ac8d134208867b6abda6e81527bf
Johnny McG <johnnymcg@outlook.com>
2021-03-22 18:03:29 -0700

    initial commit with basic math functions and html

---
+++ main.js
@@ -0,0 +1,13 @@
+//operations for calculator 
+function add (num1, num2){
+  return num1 + num2;
+};
+function subtract (num1, num2){
+  return num1 - num2;
+};
+function multiply (num1, num2){
+  return num1 * num2;
+};
+function divide (num1, num2){
+  return parseInt(num1) / parseInt(num2);
+};
