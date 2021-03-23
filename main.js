8bbfb924ee18864b3b795a652a7a5cd2d1d0748b
Johnny McG <johnnymcg@outlook.com>
2021-03-22 18:31:53 -0700

    add operation function

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ function multiply (num1, num2){
 function divide (num1, num2){
   return parseInt(num1) / parseInt(num2);
 };
+//this function takes an operator and 2 numbers 
+//and then calls one of the above functions on the numbers
+function operate(operator, num1, num2){
+  switch (operator){
+      case "+" :
+      return add(num1, num2);
+      break;
+      case "-":
+      return subtract(num1, num2);
+      break;
+      case "*":
+      return multiply(num1, num2);
+      break;
+      case "/":
+      return divide(num1, num2);
+      break;
+  };
+};
